7bd8044d9ba65e7b0cf93b5af5e0923cfc0646fa
bozhen964 <bozhencomputer964@gmail.com>
2022-10-23 01:52:43 +0300

    manuel bootstrap with auth:ui

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+
+
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
     }
 }
