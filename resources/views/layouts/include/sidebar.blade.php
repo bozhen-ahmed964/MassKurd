a77445d5fcb3e7cba9c6d5ff2c494cc0adc13073
bozhen964 <bozhencomputer964@gmail.com>
2022-10-27 16:09:17 +0300

    logout btn set

--- resources/views/layouts/include/sidebar.blade.php
+++ resources/views/layouts/include/sidebar.blade.php
@@ -119,7 +119,13 @@
 
 
               <li class="nav-item">
-                  <a class="nav-link text-white " href="#">
+                  <a class="nav-link text-white " href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                          @csrf
+                      </form>
+
                       <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                           <i class="material-icons opacity-10">login</i>
                       </div>
