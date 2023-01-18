ed9bb432b5b39d8f47ebf118102f7a00262f37f1
bozhen964 <bozhencomputer964@gmail.com>
2023-01-18 21:46:33 +0300

    adding some card to dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,12 +3,97 @@
 
 
 @section('content')
-<div class="card">
-    <div class="card-body">
-        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+    <div class="card">
+        <div class="card-body">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                 <div class="bg-gradient-dark shadow-dark border-radius-lg pt-4 pb-3 my-4">
                     <h3 class="text-white text-capitalize ps-3 ">Dashboard</h3>
                 </div>
-    </div>
-</div>
-@endsection
+            </div>
+            <div class="container-fluid py-4">
+                <div class="row">
+                    {{-- 1 --}}
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card shadow-dark">
+                            <div class="card-header p-3 pt-2">
+                                <div
+                                    class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                                    <i class="material-icons opacity-10">weekend</i>
+                                </div>
+                                <div class="text-end pt-1">
+                                    <p class="text-sm mb-0 text-capitalize">Today's Money</p>
+                                    <h4 class="mb-0">$53k</h4>
+                                </div>
+                            </div>
+                            <hr class="dark horizontal my-0">
+                            <div class="card-footer p-3">
+                                <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than
+                                    last week</p>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- 2 --}}
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card shadow-dark">
+                            <div class="card-header p-3 pt-2">
+                                <div
+                                    class="icon icon-lg icon-shape bg-gradient-primary shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                                    <i class="material-icons opacity-10">weekend</i>
+                                </div>
+                                <div class="text-end pt-1">
+                                    <p class="text-sm mb-0 text-capitalize">Today's Money</p>
+                                    <h4 class="mb-0">$53k</h4>
+                                </div>
+                            </div>
+                            <hr class="dark horizontal my-0">
+                            <div class="card-footer p-3">
+                                <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than
+                                    last week</p>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- 3 --}}
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card shadow-dark">
+                            <div class="card-header p-3 pt-2">
+                                <div
+                                    class="icon icon-lg icon-shape bg-gradient-success shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                                    <i class="material-icons opacity-10">weekend</i>
+                                </div>
+                                <div class="text-end pt-1">
+                                    <p class="text-sm mb-0 text-capitalize">Today's Money</p>
+                                    <h4 class="mb-0">$53k</h4>
+                                </div>
+                            </div>
+                            <hr class="dark horizontal my-0">
+                            <div class="card-footer p-3">
+                                <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than
+                                    last week</p>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- 4 --}}
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card shadow-dark">
+                            <div class="card-header p-3 pt-2">
+                                <div
+                                    class="icon icon-lg icon-shape bg-gradient-warning shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                                    <i class="material-icons opacity-10">weekend</i>
+                                </div>
+                                <div class="text-end pt-1">
+                                    <p class="text-sm mb-0 text-capitalize">Today's Money</p>
+                                    <h4 class="mb-0">$53k</h4>
+                                </div>
+                            </div>
+                            <hr class="dark horizontal my-0">
+                            <div class="card-footer p-3">
+                                <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than
+                                    last week</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    @endsection
