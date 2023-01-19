d65da83e4a09e5d4932c456aa8e0ca5a059c8cc2
bozhen964 <bozhencomputer964@gmail.com>
2023-01-19 05:05:34 +0300

    cal calculator done

--- resources/views/admin/Calculator.blade.php
+++ resources/views/admin/Calculator.blade.php
@@ -14,24 +14,28 @@
                 <div class="col-4">
                     {{-- calc input --}}
                     <div class="card-body">
-                        <h3>Input Section</h3>
+                        <h3>Calories Calculator </h3>
                         <form method="post" action="{{ route('Calculator') }}">
                             @csrf
                             <div class="form-group">
-                                <label for="weight">Weight (kg):</label>
-                                <input type="text" name="weight" class="form-control">
-                            </div>
+                                <label style="font-size: 22px;">Weight (kg):</label>
+                                <input type="text" name="weight" class="form-control form-control-lg shadow-dark"
+                                    style="font-size: 22px;">
+                            </div> <br><br>
                             <div class="form-group">
-                                <label for="height">Height (cm):</label>
-                                <input type="text" name="height" class="form-control">
-                            </div>
+                                <label style="font-size: 22px;">Height (cm):</label>
+                                <input type="text" name="height" class="form-control form-control-lg shadow-dark"
+                                    style="font-size: 22px;">
+                            </div><br><br>
                             <div class="form-group">
-                                <label for="age">Age:</label>
-                                <input type="text" name="age" class="form-control">
-                            </div>
+                                <label style="font-size: 22px;">Age:</label>
+                                <input type="text" name="age" class="form-control form-control-lg shadow-dark"
+                                    style="font-size: 22px;">
+                            </div><br><br>
                             <div class="form-group">
-                                <label for="activity">Activity Level:</label>
-                                <select name="activity" class="form-control">
+                                <label style="font-size: 22px;">Activity Level:</label>
+                                <select name="activity" class="form-control form-control-lg shadow-dark"
+                                    style="font-size: 22px;">
                                     <option value="1.2">Sedentary (little or no exercise)</option>
                                     <option value="1.375">Lightly Active (light exercise or sports 1-3 days a week)
                                     </option>
@@ -41,18 +45,48 @@
                                     <option value="1.9">Super Active (very hard exercise or sports and physical job or
                                         training 2 times a day)</option>
                                 </select>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Calculate</button>
+                            </div><br><br>
+                            <button type="submit" class="btn btn-primary btn-lg">Calculate</button>
                         </form>
                     </div>
                 </div>
                 <div class="col-6">
                     {{-- calc input --}}
-                    <div class="card-body">
-                        <h3>Output Section & Guide</h3>
-                        <h1>{{ $result }}</h1>
+                    <div class="card-body shadow-dark">
+                        <h3>CAL Calculator</h3>
+                        <h1>result here</h1>
+                        <ul>
+                            <h4>Activity Level</h4>
+                            <li>
+                                Sedentary (little or no exercise) = <strong class="text-lg"
+                                    style="color: black;font-size:16px;">1.2</strong>
+                            </li>
+                            <li>
+                                Lightly Active (light exercise or sports 1-3 days a week) = <strong class="text-lg"
+                                    style="color: black;font-size:16px;">1.375</strong>
+                            </li>
+                            <li>
+                                Moderately Active (moderate exercise or sports 3-5 days a week) = <strong class="text-lg"
+                                    style="color: black;font-size:16px;">1.55</strong>
+                            </li>
+                            <li>
+                                Very Active (hard exercise or sports 6-7 days a week) = <strong class="text-lg"
+                                    style="color: black;font-size:16px;">1.725</strong>
+                            </li>
+                            <li>
+                                Super Active (very hard exercise or sports and physical job or
+                                training 2 times a day) = <strong class="text-lg"
+                                    style="color: black;font-size:16px;">1.9</strong>
+                            </li>
+                        </ul>
+                        <br>
+                        <br>
+                        <h4> Harris-Benedict Equation</h4>
+                        <h3> BMR = 66 + (13.7 * Weight) + (5 * Height) - (6.8 * Age)
+                            CAL = BMR * Activity</h3>
                     </div>
                 </div>
+                <div class="card-body shadow-dark"></div>
             </div>
 
         </div>
