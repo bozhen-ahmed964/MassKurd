04ca40fb8a66d1ab1502dc282a0928f98ac9d1f2
bozhen964 <bozhencomputer964@gmail.com>
2023-01-30 00:47:20 +0300

    removing vueJs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,4 @@
 import './bootstrap';
-import Vue from 'vue';
-import ExampleComponent from './components/ExampleComponent.vue';
 
 
-const app = new Vue({
-    el: '#app',
-    components: {
-        ExampleComponent
-    }
-});
+
--- resources/views/components/example-component.blade.vue
+++
@@ -1,3 +0,0 @@
-<div id="app">
-   <example-component></example-component><!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
-</div>
