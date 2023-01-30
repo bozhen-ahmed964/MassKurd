40859fb61dae9a683d7cec9062c4b1780f416457
bozhen964 <bozhencomputer964@gmail.com>
2023-01-30 04:01:17 +0300

    fixing navbar showing user + date + time

--- resources/views/layouts/include/navbar.blade.php
+++ resources/views/layouts/include/navbar.blade.php
@@ -1,3 +1,32 @@
-     {{-- <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
-      <h1>Hello world</h1>
-    </nav> --}}
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"data-scroll="true">
+    <h3>Bozhen Ahmed</h3>
+    <h3 class="mx-4"><?php echo date('Y-m-d', strtotime('now')); ?></h3>
+    <h3 id="time">00:00:00 PM</h3>
+
+</nav>
+<script>
+    const time = document.getElementById('time');
+    // show Time
+    function showTime() {
+        let today = new Date(),
+            hour = today.getHours(),
+            min = today.getMinutes(),
+            sec = today.getSeconds();
+
+        // set AM PM
+        const amPm = hour >= 12 ? 'PM' : 'Am';
+
+        // 12h format
+        hour = hour % 12 || 12;
+
+        // output
+        time.innerHTML = (`${hour}<span>:</span>${zeros(min)}<span>:</span>${zeros(sec)}`);
+        setTimeout(showTime, 1000);
+    }
+    showTime();
+
+    // adding zeros
+    function zeros(n) {
+        return (parseInt(n, 10) < 10 ? '0' : '') + n;
+    }
+</script>
