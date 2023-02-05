f1d182dbd0a5151c8e13ae3486adc789f93c0709
bozhen964 <bozhencomputer964@gmail.com>
2023-02-06 02:25:42 +0300

    adding exercise detail from database

--- resources/views/admin/exerciseDetail.blade.php
+++ resources/views/admin/exerciseDetail.blade.php
@@ -10,11 +10,44 @@
                     <h3 class="text-white text-capitalize ps-3 ">Exercise Detail</h3>
                 </div>
             </div>
-            <h3>{{ $exerciseData->Exercise_Name }}</h3>
-            <h3>{{ $exerciseData->Primary_Muscle }}</h3>
-            <img src="{{ asset('images/' . $exerciseData->image) }}"
+            {{--
+            <img
                 class="rounded img-fluid border-3 border-primary shadow d-block w-100 fit-cover"
-                style="height: 360px;width: 360px;background: center / cover no-repeat;" />
+                style="height: 360px;width: 360px;background: center / cover no-repeat;" /> --}}
+            <section class="position-relative py-4 py-xl-5" style="height: 715px;">
+                <div class="container position-relative">
+                    <div class="row d-flex justify-content-center" style="height: 715px;">
+                        <div class="col-md-6 col-lg-4 col-xl-4" style="width: 760px;height: 715px;">
+                            <h1>{{ $exerciseData->Exercise_Name }}</h1>
+                            <h4>{{ $exerciseData->Primary_Muscle }}</h4>
+                            <p style="height: 620px;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+                                repellat doloribus ea eius porro beatae reprehenderit enim illo ratione deleniti quisquam
+                                illum perspiciatis minus sit, repudiandae blanditiis amet a magnam temporibus sequi
+                                exercitationem tempore unde. Soluta ex in dolorem modi voluptas corporis delectus beatae
+                                iste voluptates. Voluptatum velit alias excepturi recusandae facilis ipsa vero aut. Amet
+                                repellat nulla optio voluptates nostrum quas. Repellendus, maxime cumque? Ea recusandae
+                                eligendi autem pariatur, hic, libero, ipsa consectetur ratione soluta error at. Voluptates,
+                                veniam error nam adipisci rerum possimus labore expedita repellat unde veritatis id tempore
+                                modi amet accusamus. Tenetur similique natus eveniet perspiciatis!<br /></p>
+                        </div>
+                        <div class="col-md-6 col-lg-5 col-xl-4" style="width: 380px;height: 715px;"><img
+                                src="{{ asset('images/' . $exerciseData->image) }}" class="border rounded border-3 shadow"
+                                style="width: 360px;height: 360px;background: center / cover no-repeat;" />
+                            <div style="margin-top: 10px;">
+                                <h5>information</h5>
+                                <div class="progress border rounded shadow">
+                                    <div class="progress-bar bg-primary progress-bar-animated" aria-valuenow="50"
+                                        aria-valuemin="0" aria-valuemax="100" style="width: 50%;"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
 @endsection
+
+
+
+
