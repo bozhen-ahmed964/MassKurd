10f391d677b290ae64626c0e63bcde9b5a8fbe4f
bozhen964 <bozhencomputer964@gmail.com>
2023-03-31 13:58:53 +0300

    redesign billing page

--- resources/views/admin/billing.blade.php
+++ resources/views/admin/billing.blade.php
@@ -49,123 +49,124 @@
 </style>
 
 @section('content')
-    <div class="row">
-        <div class="col">
-            <div class="row">
-                <div class="col-xl-6">
-                    <div class="row">
-                        {{-- fisrt square --}}
-                        <div class="col-md-6 col-6">
-                            <div class="card shadow-dark">
-                                <div class="card-header mx-4 p-3 text-center">
-                                    <div
-                                        class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
-                                        <i class="material-icons opacity-10">account_balance</i>
-                                    </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="bg-gradient-dark shadow-dark border-radius-lg pt-4 pb-3 my-4">
+                    <h3 class="text-white text-capitalize ps-3 ">Dashboard</h3>
+                </div>
+            </div>
+            <div class="container-fluid py-4">
+                {{-- first row --}}
+                <div class="row">
+                    {{-- Total Member Salary Current --}}
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card shadow-dark">
+                            <div class="card-header p-3 pt-2">
+                                <div
+                                    class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                                    <i class="material-icons opacity-10">person</i>
                                 </div>
-                                <div class="card-body pt-0 p-3 text-center">
-                                    <h6 class="text-center mb-0" style="font-size: 24px;">Total Member Salaray Last Month
-                                    </h6>
-                                    <span class="text-xs">Belong Interactive</span>
-                                    <hr class="horizontal dark my-3">
-                                    <h5 class="mb-0" style="font-size: 26px;">{{ $totalMemberSalarayLastMonth }} IQD</h5>
-                                </div>
-                            </div>
-                        </div>
-                        {{-- second square --}}
-                        <div class="col-md-6 col-6">
-                            <div class="card shadow-dark">
-                                <div class="card-header mx-4 p-3 text-center">
-                                    <div
-                                        class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
-                                        <i class="material-icons opacity-10">account_balance_wallet</i>
-                                    </div>
-                                </div>
-                                <div class="card-body pt-0 p-3 text-center">
-                                    <h6 class="text-center mb-0" style="font-size: 24px;">Total Member Salaray By This Year
-                                    </h6>
-                                    <span class="text-xs">Freelance Payment</span>
-                                    <hr class="horizontal dark my-3">
-                                    <h5 class="mb-0" style="font-size: 26px;">{{ $totalMemberSalarayCurrentMonth }} IQD
-                                    </h5>
+                                <div class="text-end pt-1">
+                                    <strong class="text-sm mb-0 text-capitalize">Total Member Salary Current </strong>
+                                    <h4 class="mb-0">{{ $totalMemberSalarayCurrent }}</h4>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                {{-- third square --}}
-                <div class="col-md-3">
-                    <div class="card shadow-dark">
-                        <div class="card-header mx-4 p-3 text-center">
-                            <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
-                                <i class="material-icons opacity-10">account_balance_wallet</i>
+                    {{-- Total member Salary Last Month --}}
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card shadow-dark">
+                            <div class="card-header p-3 pt-2">
+                                <div
+                                    class="icon icon-lg icon-shape bg-gradient-primary shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                                    <i class="material-icons opacity-10">person</i>
+                                </div>
+                                <div class="text-end pt-1">
+                                    <strong class="text-sm mb-0 text-capitalize">Total member Salary Last Month </strong>
+                                    <h4 class="mb-0">{{ $totalMemberSalarayLastMonth }}</h4>
+                                </div>
                             </div>
                         </div>
-                        <div class="card-body pt-0 p-3 text-center">
-                            <h6 class="text-center mb-0" style="font-size: 24px;">Total Member Salaray Now</h6>
-                            <span class="text-xs">Freelance Payment</span>
-                            <hr class="horizontal dark my-3">
-                            <h5 class="mb-0" style="font-size: 26px;">{{ $totalMemberSalarayCurrent }} IQD</h5>
+                    </div>
+                    {{-- Total member Salary Current Month --}}
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card shadow-dark">
+                            <div class="card-header p-3 pt-2">
+                                <div
+                                    class="icon icon-lg icon-shape bg-gradient-success shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                                    <i class="material-icons opacity-10">person</i>
+                                </div>
+                                <div class="text-end pt-1">
+                                    <strong class="text-sm mb-0 text-capitalize">Total member Salary Current Month</strong>
+                                    <h4 class="mb-0">{{ $totalMemberSalarayCurrentMonth }}</h4>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    {{-- Billing Information --}}
-    <div class="row mt-5">
-        <div class="border rounded">
-            <table class="table table-hover shadow-dark align-items-center">
-                <thead style="font-size:18px;background-color:#494949d5;color:white;">
-                    <th>Full Name</th>
-                    <th>Age</th>
-                    <th>Gender</th>
-                    <th>Game Type</th>
-                    <th>Pay</th>
-                    <th>Course Pay</th>
-                    <th>Join Date</th>
-                    <th>Expire Date</th>
-                </thead>
-                <tbody style="font-weight: bold; font-size:20px; text-align:left; color:black;">
-                    @foreach ($memberHistory as $history)
-                        <tr>
-                            <td>{{ $history->Full_Name }}</td>
-                            <td>{{ $history->Age }}</td>
-                            <td>{{ $history->Gender }}</td>
-                            <td>{{ $history->Game_Type }} </td>
-                            <td>{{ $history->Pay }} IQD</td>
-                            <td>{{ $history->Course_Pay }} IQD</td>
-                            <td>{{ $history->created_at }}</td>
-                            <td>{{ $history->updated_at }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <div class="pagination  ">
-                <span class="page-number">Page {{ $memberHistory->currentPage() }} of
-                    {{ $memberHistory->lastPage() }}</span>
-                <ul class="pagination-links">
-                    @if ($memberHistory->onFirstPage())
-                        <li class="disabled"><span>&laquo;</span></li>
-                    @else
-                        <li><a href="{{ $memberHistory->previousPageUrl() }}" rel="prev">&laquo;</a></li>
-                    @endif
 
-                    @foreach ($memberHistory->getUrlRange(1, $memberHistory->lastPage()) as $page => $url)
-                        @if ($page == $memberHistory->currentPage())
-                            <li class="active"><span>{{ $page }}</span></li>
+
+
+
+
+
+        {{-- Billing Information --}}
+        <div class="row mt-5">
+            <div class="border rounded">
+                <table class="table table-hover shadow-dark align-items-center">
+                    <thead style="font-size:18px;background-color:#494949d5;color:white;">
+                        <th>Full Name</th>
+                        <th>Age</th>
+                        <th>Gender</th>
+                        <th>Game Type</th>
+                        <th>Pay</th>
+                        <th>Course Pay</th>
+                        <th>Join Date</th>
+                        <th>Expire Date</th>
+                    </thead>
+                    <tbody style="font-weight: bold; font-size:20px; text-align:left; color:black;">
+                        @foreach ($memberHistory as $history)
+                            <tr>
+                                <td>{{ $history->Full_Name }}</td>
+                                <td>{{ $history->Age }}</td>
+                                <td>{{ $history->Gender }}</td>
+                                <td>{{ $history->Game_Type }} </td>
+                                <td>{{ $history->Pay }} IQD</td>
+                                <td>{{ $history->Course_Pay }} IQD</td>
+                                <td>{{ $history->created_at }}</td>
+                                <td>{{ $history->updated_at }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="pagination  ">
+                    <span class="page-number">Page {{ $memberHistory->currentPage() }} of
+                        {{ $memberHistory->lastPage() }}</span>
+                    <ul class="pagination-links">
+                        @if ($memberHistory->onFirstPage())
+                            <li class="disabled"><span>&laquo;</span></li>
                         @else
-                            <li><a href="{{ $url }}">{{ $page }}</a></li>
+                            <li><a href="{{ $memberHistory->previousPageUrl() }}" rel="prev">&laquo;</a></li>
                         @endif
-                    @endforeach
 
-                    @if ($memberHistory->hasMorePages())
-                        <li><a href="{{ $memberHistory->nextPageUrl() }}" rel="next">&raquo;</a></li>
-                    @else
-                        <li class="disabled"><span>&raquo;</span></li>
-                    @endif
-                </ul>
+                        @foreach ($memberHistory->getUrlRange(1, $memberHistory->lastPage()) as $page => $url)
+                            @if ($page == $memberHistory->currentPage())
+                                <li class="active"><span>{{ $page }}</span></li>
+                            @else
+                                <li><a href="{{ $url }}">{{ $page }}</a></li>
+                            @endif
+                        @endforeach
+
+                        @if ($memberHistory->hasMorePages())
+                            <li><a href="{{ $memberHistory->nextPageUrl() }}" rel="next">&raquo;</a></li>
+                        @else
+                            <li class="disabled"><span>&raquo;</span></li>
+                        @endif
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
