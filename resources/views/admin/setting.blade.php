81dc52a8ed70037266fafdcaf0100ba60332da9a
bozhen964 <bozhencomputer964@gmail.com>
2023-05-19 15:26:47 +0300

    setting page fixing design

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -4,14 +4,14 @@
 
 
 @section('content')
-    <div class="card">
-        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+
+        <div class="card-header p-0 position-relative mt-2 mx-3 z-index-2 ">
             <div class="bg-gradient-dark shadow-dark border-radius-lg pt-4 pb-3 my-4">
                 <h3 class="text-white text-capitalize ps-3 ">Setting</h3>
             </div>
         </div>
-        <div class="card-body shadow-dark">
-            <table class="table table-hover shadow-dark align-items-center">
+
+            <table class="table table-hover align-items-center mt-5 ">
                 <tbody style="font-weight: bold; font-size:20px;color:black;">
                     <tr>
                         <td>Deleting All Members In Active Table</td>
@@ -27,6 +27,5 @@
                     </tr>
                 </tbody>
             </table>
-        </div>
-    </div>
+
 @endsection
