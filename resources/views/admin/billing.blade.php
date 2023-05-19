c42dd151881aa1be407c03ae29aab9af4b995c42
bozhen964 <bozhencomputer964@gmail.com>
2023-05-19 15:36:56 +0300

    small fix in design bill page

--- resources/views/admin/billing.blade.php
+++ resources/views/admin/billing.blade.php
@@ -53,7 +53,7 @@
         <div class="card-body">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                 <div class="bg-gradient-dark shadow-dark border-radius-lg pt-4 pb-3 my-4">
-                    <h3 class="text-white text-capitalize ps-3 ">Dashboard</h3>
+                    <h3 class="text-white text-capitalize ps-3 ">Billing</h3>
                 </div>
             </div>
             <div class="container-fluid py-4">
