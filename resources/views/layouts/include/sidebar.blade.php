7499c3ced478fa59c31577356ed54852d474526f
bozhen964 <bozhencomputer964@gmail.com>
2023-06-07 22:26:26 +0300

    spell corrcetion

--- resources/views/layouts/include/sidebar.blade.php
+++ resources/views/layouts/include/sidebar.blade.php
@@ -40,7 +40,7 @@
                       <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                           <i class="material-icons opacity-10">table_view</i>
                       </div>
-                      <span class="nav-link-text ms-1">Members Tabel</span>
+                      <span class="nav-link-text ms-1">Members Table</span>
                   </a>
               </li>
 
